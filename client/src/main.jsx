b5e0cb42a22eca83824ba25d2c543820edda63ec
Md Emamudin <mdemamudin726@gmail.com>
2024-08-04 17:29:58 +0530

    added redux persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { store } from './redux/store.js'
+import { store , persistor} from './redux/store.js'
 import {Provider} from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+
+// if we dont use persist-redux then the states of redux becomes null if we refresh page so to get value even after refreshing we use persist-redux that stores and gets value from local storage
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
     <App />
+    </PersistGate>
   </Provider>,
 )
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,11 +1,28 @@
-import {configureStore} from '@reduxjs/toolkit'
+import {combineReducers, configureStore} from '@reduxjs/toolkit'
 import userReducer from '../redux/user/userSlice.js'
+import {persistReducer, persistStore} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const rootReducer = combineReducers({user : userReducer})
+
+const persistConfig = {
+    key: "root",
+    storage,
+    version:1,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-    reducer:{
-        user : userReducer,
-    },
+    // reducer:{
+    //     user : userReducer,  // if we add reducer like this it becomes null if we refresh so to get value even after refreshing we use persist Reducer that stores and gets value from local storage
+        
+    // },
+    reducer : persistedReducer,
     middleware:(getDefaultMiddleware)=>
         getDefaultMiddleware({
             serializableCheck:false,
         }),
 })
+
+export const persistor = persistStore(store)
