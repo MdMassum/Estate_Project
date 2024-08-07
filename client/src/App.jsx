b2691118849f45a4ebb9672248ba9033455f0280
Md Emamudin <mdemamudin726@gmail.com>
2024-08-07 22:40:18 +0530

    created listing page ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import Header from './components/Header'
 import PrivateRoute from './components/PrivateRoute'
 import CreateListing from './pages/CreateListing'
 import UpdateListing from './pages/UpdateListing'
+import Listing from './pages/Listing'
 
 
 function App() {
@@ -21,6 +22,7 @@ function App() {
           <Route path='/about' element={<About /> } ></Route>
           <Route path='/sign-in' element={<SignIn /> } ></Route>
           <Route path='/sign-up' element={<SignUp /> } ></Route>
+          <Route path='/listing/:listingId' element={<Listing /> } ></Route>
           <Route element={<PrivateRoute />}>
               <Route path='/profile' element={<Profile /> } ></Route>
               <Route path='/create-listing' element={<CreateListing /> } ></Route>
