8076f1a8a70e1a181fd1eb8f21f2e72f7110d153
Md Emamudin <mdemamudin726@gmail.com>
2024-10-30 14:05:06 +0530

    added backend url in proxy server

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server:{
     proxy:{
       "/api":{
-        target: 'http://localhost:3000',
+        target: 'https://estate-backend-blond.vercel.app',
         secure: false,
       },
     },
